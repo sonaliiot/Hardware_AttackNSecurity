685c2561be461cf1d84564b81744d501e91dfe6f
Neelam Sharma <sapphire.sharma1996@gmail.com>
2023-04-22 23:38:52 +0530

    corrected the lfsr seq generator seq code

--- lfsr_generator.py
+++ lfsr_generator.py
@@ -1,15 +1,14 @@
-
-myseq = [1, 1, 0] #initial seed
-coeff = [1,1,1]
+myseq = [0, 1, 0,0] #initial seed
+coeff = [0,0,0,1,1] # extra element at index 0. Take from c1; c0=0 is dummy to take coeff indices from c1; coeff = [0,0,0,1,1] represents (x^4 + x + 1)
 start=0
-lfsr_len = len(coeff)
+lfsr_len = len(coeff)-1
 #print(myseq)
 seq_length = 10   #change this to the value for how many sequence values you want after seed
 
 for i in range(seq_length):
     nextbit=0
-    for j in range(start, start+lfsr_len):
-        nextbit^=(coeff[j-start]*myseq[j])
+    for j in range(0, lfsr_len):
+        nextbit^=(coeff[j+1]*myseq[start+lfsr_len-j-1])
     start+=1
     myseq.append(nextbit)
 
